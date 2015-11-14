177a6f7487d31f451198d16528276d42933a748d
Darky0404 <darky@koptevo.net>
2015-11-15 03:22:48 +0400

    added alf pr filez

--- IDEA/test/alf/src/alf.java
+++ IDEA/test/alf/src/alf.java
@@ -1,10 +1,18 @@
 /**
- * Сделано
- * Sergey в
+ *     Created
+ *      Sergey
  * 13.Ноябрь.2015г.
  * 4:01
  **/
 
 
 public class alf {
+      public static void main(String[] args) {
+//        inp.on();
+        inp.inp();
+
+
+
+
+    }
 }
