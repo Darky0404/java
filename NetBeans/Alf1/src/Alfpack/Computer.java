9400f25cd461573e0c6ba915adf6ce5583d61b0a
Darky0404 <darky@koptevo.net>
2015-11-15 04:47:16 +0400

    Alitest new flz added

---
+++ NetBeans/Alf1/src/Alfpack/Computer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Alfpack;
+
+/**
+ *
+ * @author Sergey
+ */
+public class Computer {
+    
+    private String name;
+    private int ram;
+    private int hdd;
+    
+    public static void on() {
+            System.out.println("Включаюсь...!");
+    }
+    public static void off() {
+            System.out.println("Выключаюсь...!");
+    }
+}
